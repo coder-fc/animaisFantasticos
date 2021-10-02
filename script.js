a0436475f413a70665c5ce949b4870fee303da4a
Felipe Costa Developer <felipech3344@gmail.com>
2021-10-02 00:48:28 -0300

    Document Scroll Suave
    Criação das funções para suavizar o scroll para os links internos do site, para não termos a sensação de estar saindo da pagina.

--- script.js
+++ script.js
@@ -42,3 +42,31 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSuave(){
+    const linksInternos =  document.querySelectorAll('.js-menu a[href^="#"]'); 
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href); 
+        
+        section.scrollIntoView({
+            behavior:'smooth',
+            block: 'start',
+        });
+
+        /* Forma alternativa */
+        /*  const topo = section.offsetTop;
+            window.scrollTo({
+            top: topo,
+            behavior: 'smooth',
+        }); */ 
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+
+initScrollSuave();
