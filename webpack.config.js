8d29c223f1c5b030e5ea1c26554521144c2e1f01
Felipe Costa <felipech3344@gmail.com>
2022-03-01 12:20:07 -0300

    Configurações web pack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        esclude: /node_modules/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
