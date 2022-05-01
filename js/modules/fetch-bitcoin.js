7b88fd927c1814d1758818b2115da2c09d2e25ff
Felipe Costa <felipech3344@gmail.com>
2022-04-30 21:13:38 -0300

    Refatoração fetch bit coin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
-    .then((json) => {
-      const bitcoin = document.querySelector('.btc-preco');
-      bitcoin.innerText = (1000 / json.BRL.sell).toFixed(4);
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
     }).catch((erro) => {
       console.log(Error(erro));
     });
